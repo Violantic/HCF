4bd0f820c657f69958ec9e099103a41b7f73fb62
Ziluck, Michael H [EH&S] <ehs_ziluckm@iastate.edu>
2017-06-16 11:01:35 -0500

    Convert FStat to new command API

--- src/main/java/me/borawski/hcf/command/commands/FStatCommand.java
+++ src/main/java/me/borawski/hcf/command/commands/FStatCommand.java
@@ -1,89 +1,58 @@
 package me.borawski.hcf.command.commands;
 
-import com.massivecraft.factions.listeners.FactionsPlayerListener;
-import me.borawski.hcf.command.Command;
-import me.borawski.hcf.session.FSession;
-import me.borawski.hcf.session.Rank;
-import me.borawski.hcf.session.Session;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.massivecraft.factions.Factions;
+import com.massivecraft.factions.entity.MPlayer;
+
+import me.borawski.hcf.Core;
+import me.borawski.hcf.command.CustomBaseCommand;
+import me.borawski.hcf.session.FSession;
+import me.borawski.hcf.session.Rank;
+import me.borawski.hcf.session.Session;
+
 /**
  * Created by Ethan on 5/7/2017.
  */
-public class FStatCommand implements Command {
+public class FStatCommand extends CustomBaseCommand {
 
-    @Override
-    public String getName() {
-        return "fstat";
+    public FStatCommand() {
+        super("fstat", "View your player stats", Rank.GUEST, "fstats");
     }
 
     @Override
-    public Rank requiredRank() {
-        return Rank.GUEST;
-    }
-
-    @Override
-    public void execute(CommandSender sender, String[] args) {
-        if(args.length == 0) {
-            try {
-                String faction = FactionsPlayerListener.factions.get(((Player) sender).getUniqueId());
-                FSession session = FSession.getSession(faction);
-                int score = session.getTrophies();
-                int koth = session.getKoth();
-                sender.sendMessage(getPrefix() + "Faction: " + ChatColor.YELLOW + faction);
-                sender.sendMessage(getPrefix() + "Trophy Points: " + ChatColor.YELLOW + score);
-                sender.sendMessage(getPrefix() + "KOTH Wins: " + ChatColor.YELLOW + koth);
-            } catch (Exception e) {
-                sender.sendMessage(getPrefix() + "We couldn't find your faction stats! (Maybe you're not in a faction?)");
+    public void run(CommandSender sender, String label, String[] args) {
+        if (args.length == 0) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(Core.getLangHandler().getString("only-players"));
+                return;
             }
-        } else if(args.length == 1) {
+            String faction = MPlayer.get(sender).getFactionName().replace(Factions.NAME_NONE_DEFAULT, "None");
+            FSession session = FSession.getSession(faction);
+            sender.sendMessage(getPrefix() + "Faction: " + ChatColor.YELLOW + faction);
+            sender.sendMessage(getPrefix() + "Trophy Points: " + ChatColor.YELLOW + session.getTrophies());
+            sender.sendMessage(getPrefix() + "KOTH Wins: " + ChatColor.YELLOW + session.getKoth());
+        } else {
             Session s = Session.getSession((Player) sender);
-            if(s.getRank().getId() >= Rank.MODERATOR.getId()) {
+            if (s.getRank().getId() >= Rank.MODERATOR.getId()) {
                 String faction = args[0];
                 try {
                     FSession session = FSession.getSession(faction);
-                    int score = session.getTrophies();
-                    int koth = session.getKoth();
-                    sender.sendMessage(getPrefix() + "Faction: " + ChatColor.YELLOW + faction);
-                    sender.sendMessage(getPrefix() + "Trophy Points: " + ChatColor.YELLOW + score);
-                    sender.sendMessage(getPrefix() + "KOTH Captures: " + ChatColor.YELLOW + koth);
+                    sender.sendMessage(getPrefix() + "Faction: " + ChatColor.YELLOW + session.getName());
+                    sender.sendMessage(getPrefix() + "Trophy Points: " + ChatColor.YELLOW + session.getTrophies());
+                    sender.sendMessage(getPrefix() + "KOTH Captures: " + ChatColor.YELLOW + session.getKoth());
                 } catch (Exception e) {
                     sender.sendMessage(getPrefix() + "Could not find season data for " + ChatColor.YELLOW + faction);
                 }
             } else {
-                sender.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "PERMISSIONS " + ChatColor.RESET + "" + ChatColor.GRAY + "You don't have permission to view faction stats!");
-            }
-        } else if(args.length == 3) {
-            Session s = Session.getSession((Player) sender);
-            if(s.getRank().getId() >= Rank.ADMIN.getId()) {
-                String faction = args[2];
-                try {
-                    FSession session = FSession.getSession(faction);
-                    String usage = "";
-                    if(args[0].equalsIgnoreCase("setpoints")) {
-                        Integer points = Integer.valueOf(args[1]);
-                        //Plugin.getInternal().getFactionSession().setScore(faction, "points", points);
-                        session.updateDocument("factions", "points", points);
-                        usage = "Trophy Points";
-                    } else if(args[0].equalsIgnoreCase("setkothwins")) {
-                        Integer koth = Integer.valueOf(args[1]);
-                        //Plugin.getInternal().getFactionSession().setScore(faction, "koth_wins", koth);
-                        session.updateDocument("factions", "koth", koth);
-                        usage = "KOTH Wins";
-                    }
-                    sender.sendMessage(getPrefix() + "You have updated " + ChatColor.YELLOW + usage + "" + ChatColor.GRAY + " for " + ChatColor.YELLOW + faction);
-                } catch (Exception e) {
-                    sender.sendMessage(getPrefix() + "Could not find season data for " + ChatColor.YELLOW + faction);
-                }
-            } else {
-                sender.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "PERMISSIONS " + ChatColor.RESET + "" + ChatColor.GRAY + "You don't have permission to view faction stats!");
+                sender.sendMessage(Core.getLangHandler().getString("no-permissions"));
             }
         }
     }
 
-    public static String getPrefix() {
+    private String getPrefix() {
         return ChatColor.DARK_RED + "" + ChatColor.BOLD + "FSTATS " + ChatColor.RESET + "" + ChatColor.GRAY;
     }
 }
