2ce70973f1537e6ad566aeb120b4fdf6f0b782c4
Ziluck, Michael H [EH&S] <ehs_ziluckm@iastate.edu>
2017-06-22 09:15:19 -0500

    Finish updating the RankCommand.

--- src/main/java/me/borawski/hcf/command/commands/RankCommand.java
+++ src/main/java/me/borawski/hcf/command/commands/RankCommand.java
@@ -1,13 +1,9 @@
 package me.borawski.hcf.command.commands;
 
-import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
 import me.borawski.hcf.command.CustomBaseCommand;
+import me.borawski.hcf.command.commands.rank.RankCheckCommand;
 import me.borawski.hcf.command.commands.rank.RankSetCommand;
 import me.borawski.hcf.session.Rank;
-import me.borawski.hcf.session.Session;
 
 /**
  * Created by Ethan on 3/12/2017.
@@ -15,22 +11,9 @@ import me.borawski.hcf.session.Session;
 public class RankCommand extends CustomBaseCommand {
 
     public RankCommand() {
-        super("rank", "View your rank or manage others.", Rank.ADMIN);
+        super("rank", "View your rank or manage others.", Rank.GUEST);
+        addSubCommand(new RankCheckCommand());
         addSubCommand(new RankSetCommand());
     }
 
-    public void run(CommandSender sender, String label, String[] args) {
-        if (args.length == 0) {
-            Session s = null;
-            try {
-                s = Session.getSession(((Player) sender).getUniqueId());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            sender.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "DesireHCF " + ChatColor.RESET + "" + ChatColor.GRAY + "Your rank is currently " + ChatColor.YELLOW + s.getRank().getDisplayName());
-        } else {
-            super.run(sender, label, args);
-        }
-    }
 }
--- src/main/java/me/borawski/hcf/command/commands/rank/RankCheckCommand.java
+++ src/main/java/me/borawski/hcf/command/commands/rank/RankCheckCommand.java
@@ -1,5 +1,35 @@
 package me.borawski.hcf.command.commands.rank;
 
-public class RankCheckCommand {
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import me.borawski.hcf.Core;
+import me.borawski.hcf.command.CustomCommand;
+import me.borawski.hcf.session.Rank;
+import me.borawski.hcf.session.Session;
+
+public class RankCheckCommand extends CustomCommand {
+
+    public RankCheckCommand() {
+        super("check", "Check your rank.", Rank.GUEST);
+    }
+
+    @Override
+    public void run(CommandSender sender, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(Core.getLangHandler().getString("only-players"));
+            return;
+        }
+
+        Session s = null;
+        try {
+            s = Session.getSession(((Player) sender).getUniqueId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        sender.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "DesireHCF " + ChatColor.RESET + "" + ChatColor.GRAY + "Your rank is currently " + ChatColor.YELLOW + s.getRank().getDisplayName());
+    }
 
 }
