2b74e2dded180dd579add6376ba30efb323ced15
Michael Ziluck <ziluckmichael@gmail.com>
2017-06-21 21:10:44 -0500

    Supress warnings, reformat, and apply singleton.

--- src/main/java/me/borawski/hcf/gui/custom/settings/PlayerSettingsGUI.java
+++ src/main/java/me/borawski/hcf/gui/custom/settings/PlayerSettingsGUI.java
@@ -18,16 +18,16 @@ import org.bukkit.inventory.ItemStack;
 public class PlayerSettingsGUI extends ItemGUI {
 
     private static volatile int[][] gui = {
-            {3, 3, 3, 3, 1, 3, 3, 3, 3},
-            {3, 3, 3, 3, 3, 3, 3, 3, 3},
-            {3, 3, 3, 2, 3, 6, 3, 3, 3},
-            {3, 3, 3, 4, 3, 12, 3, 3, 3},
-            {3, 3, 3, 3, 3, 3, 3, 3, 3},
-            {3, 3, 3, 3, 3, 3, 3, 3, 3}
+            { 3, 3, 3, 3, 1, 3, 3, 3, 3 },
+            { 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { 3, 3, 3, 2, 3, 6, 3, 3, 3 },
+            { 3, 3, 3, 4, 3, 12, 3, 3, 3 },
+            { 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { 3, 3, 3, 3, 3, 3, 3, 3, 3 }
     };
 
     public PlayerSettingsGUI(Core instance, Player p) {
-        super(instance, null, p, 54);
+        super(null, p, 54);
     }
 
     @Override
@@ -40,6 +40,7 @@ public class PlayerSettingsGUI extends ItemGUI {
         return false;
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public void registerItems() {
         Session s = Session.getSession(getPlayer());
@@ -65,7 +66,7 @@ public class PlayerSettingsGUI extends ItemGUI {
 
                         }
                     }));
-                } else if(gui[x][y] == 6) {
+                } else if (gui[x][y] == 6) {
                     set(i, new MenuItem(new CustomIS().setMaterial(Material.BOOK).setName(ChatColor.YELLOW + "Private Messaging").addLore(ChatColor.GRAY + "Allows users to send you messages"), new Runnable() {
                         @Override
                         public void run() {
@@ -78,24 +79,24 @@ public class PlayerSettingsGUI extends ItemGUI {
                  * Settings change buttons
                  */
 
-                else if(gui[x][y] == 4) {
-                    String label = (s.getSettings().get("friend_requests").equals("true"))?ChatColor.GREEN + "[ENABLED]":ChatColor.RED + "[DISABLED]";
-                    ItemStack stack = new CustomIS().setMaterial(Material.WOOL).setData((s.getSettings().get("friend_requests").equals("true"))? DyeColor.LIME.getWoolData():DyeColor.RED.getWoolData()).get();
+                else if (gui[x][y] == 4) {
+                    String label = (s.getSettings().get("friend_requests").equals("true")) ? ChatColor.GREEN + "[ENABLED]" : ChatColor.RED + "[DISABLED]";
+                    ItemStack stack = new CustomIS().setMaterial(Material.WOOL).setData((s.getSettings().get("friend_requests").equals("true")) ? DyeColor.LIME.getWoolData() : DyeColor.RED.getWoolData()).get();
                     set(i, new MenuItem(new CustomIS(stack).setName(label).addLore(ChatColor.GRAY + "(Click to change)"), new Runnable() {
                         @Override
                         public void run() {
                             SettingsUtil.toggleSetting(s, "friend_requests");
-                            new PlayerSettingsGUI(getInstance(), getPlayer()).show();
+                            new PlayerSettingsGUI(Core.getInstance(), getPlayer()).show();
                         }
                     }));
-                } else if(gui[x][y] == 12) {
-                    String label = (s.getSettings().get("private_messaging").equals("true")?ChatColor.GREEN + "[ENABLED]":ChatColor.RED + "[DISABLED]");
-                    ItemStack stack = new CustomIS().setMaterial(Material.WOOL).setData((s.getSettings().get("private_messaging").equals("true"))? DyeColor.LIME.getWoolData():DyeColor.RED.getWoolData()).get();
+                } else if (gui[x][y] == 12) {
+                    String label = (s.getSettings().get("private_messaging").equals("true") ? ChatColor.GREEN + "[ENABLED]" : ChatColor.RED + "[DISABLED]");
+                    ItemStack stack = new CustomIS().setMaterial(Material.WOOL).setData((s.getSettings().get("private_messaging").equals("true")) ? DyeColor.LIME.getWoolData() : DyeColor.RED.getWoolData()).get();
                     set(i, new MenuItem(new CustomIS(stack).setName(label).addLore(ChatColor.GRAY + "(Click to change)"), new Runnable() {
                         @Override
                         public void run() {
                             SettingsUtil.toggleSetting(s, "private_messaging");
-                            new PlayerSettingsGUI(getInstance(), getPlayer()).show();
+                            new PlayerSettingsGUI(Core.getInstance(), getPlayer()).show();
                         }
                     }));
                 }
