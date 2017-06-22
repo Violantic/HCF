c2acc0168ac2b77f8d8f5e9aa14418235742d666
Michael Ziluck <ziluckmichael@gmail.com>
2017-06-21 21:15:18 -0500

    Remove inner blocks in exchange for using variables properly.

--- src/main/java/me/borawski/hcf/manual/server/YouTuberManual.java
+++ src/main/java/me/borawski/hcf/manual/server/YouTuberManual.java
@@ -22,59 +22,51 @@ public class YouTuberManual implements Manual {
 
     @Override
     public List<ManualPage> getPages() {
-        return new ArrayList<ManualPage>() {
-            {
-                add(ManualUtil.getCover(getRank()));
+        ArrayList<ManualPage> pages = new ArrayList<>();
+        pages.add(ManualUtil.getCover(getRank()));
+        pages.add(new ManualPage("Nick")
+                .addString("&4&lNICK SYSTEM")
+                .addString("\n")
+                .addString("\n" + "&7Here you can")
+                .addString("\n" + "&7Change your")
+                .addString("\n" + "&7identity")
+                .addString("\n")
+                .addButton(new ManualButton() {
+                    @Override
+                    public String getName() {
+                        return "[Set Nick]";
+                    }
 
-                // Nickname System //
-                add(new ManualPage("Nick")
-                        .addString("&4&lNICK SYSTEM")
-                        .addString("\n")
-                        .addString("\n" + "&7Here you can")
-                        .addString("\n" + "&7Change your")
-                        .addString("\n" + "&7identity")
-                        .addString("\n")
-                        .addButton(new ManualButton() {
-                            @Override
-                            public String getName() {
-                                return "[Set Nick]";
-                            }
+                    @Override
+                    public ChatColor getColor() {
+                        return ChatColor.GREEN;
+                    }
 
-                            @Override
-                            public ChatColor getColor() {
-                                return ChatColor.GREEN;
-                            }
+                    @Override
+                    public List<String> getCommands() {
+                        ArrayList<String> commands = new ArrayList<>();
+                        commands.add("nick start {player}");
+                        return commands;
+                    }
+                })
+                .addButton(new ManualButton() {
+                    @Override
+                    public String getName() {
+                        return "[No Nick]";
+                    }
 
-                            @Override
-                            public List<String> getCommands() {
-                                return new ArrayList<String>() {
-                                    {
-                                        add("nick start {player}");
-                                    }
-                                };
-                            }
-                        })
-                        .addButton(new ManualButton() {
-                            @Override
-                            public String getName() {
-                                return "[No Nick]";
-                            }
+                    @Override
+                    public ChatColor getColor() {
+                        return ChatColor.RED;
+                    }
 
-                            @Override
-                            public ChatColor getColor() {
-                                return ChatColor.RED;
-                            }
-
-                            @Override
-                            public List<String> getCommands() {
-                                return new ArrayList<String>() {
-                                    {
-                                        add("nick remove {player}");
-                                    }
-                                };
-                            }
-                        }));
-            }
-        };
+                    @Override
+                    public List<String> getCommands() {
+                        ArrayList<String> commands = new ArrayList<>();
+                        commands.add("nick remove {player}");
+                        return commands;
+                    }
+                }));
+        return pages;
     }
 }
