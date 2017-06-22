7ab6f58ef66043ebc810ccfa5b4c7e291545ae85
Michael Ziluck <ziluckmichael@gmail.com>
2017-06-21 20:55:32 -0500

    Change SettingsCommand to use new API

--- src/main/java/me/borawski/hcf/command/commands/SettingsCommand.java
+++ src/main/java/me/borawski/hcf/command/commands/SettingsCommand.java
@@ -1,44 +1,30 @@
 package me.borawski.hcf.command.commands;
 
-import me.borawski.hcf.Core;
-import me.borawski.hcf.command.Command;
-import me.borawski.hcf.gui.custom.settings.PlayerSettingsGUI;
-import me.borawski.hcf.session.Rank;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import me.borawski.hcf.Core;
+import me.borawski.hcf.command.CustomCommand;
+import me.borawski.hcf.gui.custom.settings.PlayerSettingsGUI;
+import me.borawski.hcf.session.Rank;
+
 /**
  * Created by Ethan on 3/21/2017.
  */
-public class SettingsCommand implements Command {
+public class SettingsCommand extends CustomCommand {
 
-    private Core instance;
-
-    public SettingsCommand(Core instance) {
-        this.instance = instance;
+    public SettingsCommand() {
+        super("settings", "Change your VIP settings.", Rank.VIP);
     }
 
-    public Core getInstance() {
-        return instance;
-    }
 
     @Override
-    public String getName() {
-        return "settings";
-    }
-
-    @Override
-    public Rank requiredRank() {
-        return Rank.VIP;
-    }
-
-    @Override
-    public void execute(CommandSender sender, String[] args) {
-        if(args.length > 1) {
-            sender.sendMessage(getInstance().getPrefix() + "To change your settings, simply do " + ChatColor.YELLOW + "/settings");
+    public void run(CommandSender sender, String label, String[] args) {
+        if (args.length > 1) {
+            sender.sendMessage(Core.getInstance().getPrefix() + "To change your settings, simply do " + ChatColor.YELLOW + "/settings");
             return;
         }
-        new PlayerSettingsGUI(getInstance(), (Player) sender).show();
+        new PlayerSettingsGUI(Core.getInstance(), (Player) sender).show();
     }
 }
